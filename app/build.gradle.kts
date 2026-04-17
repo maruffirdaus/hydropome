1a245803f0478a143345a7c2210f1bff297614a4
maruffirdaus <maruffirdaus@outlook.com>
2026-04-18 01:46:55 +0700

    Add Android build workflow and configure release signing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,13 +17,22 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("GITHUB_WORKSPACE") + "/keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
     buildTypes {
         release {
+            signingConfig = signingConfigs.findByName("release")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
